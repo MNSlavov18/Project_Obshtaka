436614fb344065a39085edab6293dfca2b0978d5
MNSlavov18 <MNSlavov18@codingburgas.bg>
2021-07-07 12:03:58 +0300

    Add main menu, edit and delete functions

--- Project_Obshtaka/Project_Obshtaka.cpp
+++ Project_Obshtaka/Project_Obshtaka.cpp
@@ -1,80 +1,264 @@
-// Project_Obshtaka.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
+bool option1Check = false;
 
 struct CUSTOMER
 {
-    int Id;
-    string Username;
-    string Password;
-    string First_Name;
-    string Last_Name;
-    string Address;
-    string Student_Name;
-    int Year_of_student;
+	int Id;
+	string Username;
+	string Password;
+	string First_Name;
+	string Last_Name;
+	string Address;
+	string Student_Name;
+	int Year_of_student;
 };
-
 bool isAdmin(string UsernName, string pass)
 {
-    string admin = "admin";
-    string adminPass = "adminpass";
-        if (UsernName == admin and adminPass == pass)
-        {
-            cout << "Correct Password!\nAccess Granted!"<<endl;
-            return true;
-        }
-        else
-        {
-            cout << "Wrong Password! "<<endl;
-            return false;
-        }
+	string admin = "admin";
+	string adminPass = "adminpass";
+	if (UsernName == admin and adminPass == pass)
+	{
+		cout << "Correct Password!\nAccess Granted!" << endl;
+		return true;
+	}
+	else
+	{
+		cout << "Wrong Password! " << endl;
+		return false;
+	}
 }
-void input(CUSTOMER& customer)
+void createCustomer(CUSTOMER* customers, int& customerCount, CUSTOMER newOrder, int& maxId) {
+	newOrder.Id = maxId;
+	customers[customerCount] = newOrder;
+	customerCount++;
+	maxId++;
+}
+int getCustomerById(CUSTOMER* customers, int& customerCount, int Id)
 {
-    cin >> customer.Id;
-    cin >> customer.Username;
-    cin >> customer.Password;
-    cin >> customer.First_Name;
-    cin >> customer.Last_Name;
-    cin >> customer.Address;
-    cin >> customer.Student_Name;
-    cin>> customer.Year_of_student;
+	for (int i = 0; i < customerCount; i++)
+	{
+		if (customers[i].Id == Id)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+void updateCustomer(CUSTOMER* customers, int& customerCount, CUSTOMER newCustomer, int& maxId)
+{
+	int index = getCustomerById(customers, customerCount, maxId);
+	customers[index] = newCustomer;
 }
 
-void showCustomer(CUSTOMER& customer)
-{
-    cout << customer.Id <<" " << customer.Username <<  " " << customer.Password << " " << customer.First_Name << " " << customer.Last_Name << " " << customer.Address << " " << customer.Student_Name << " " << customer.Year_of_student << endl;
+void deleteCustomer(CUSTOMER* customer, int& customerCount, int Id) {
+
+	int index = getCustomerById(customer, customerCount, Id);
+	for (int i = index; i < customerCount - 1; i++)
+	{
+		customer[i] = customer[i + 1];
+	}
+	customerCount--;
+
 }
-void showCustomers(CUSTOMER customers[], size_t size)
+CUSTOMER getOrder(CUSTOMER* customer, int& customerCount, int Id)
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        showCustomer(customers[i]);
-    }
+	int index = getCustomerById(customer, customerCount, Id);
+	if (index != Id)
+	{
+		cout << "Enter valid ID";
+		cin >> index;
+	}
+	return customer[index];
 }
+
+
+void showCustomerMenu(CUSTOMER* customers, int& customerCount, int& maxId)
+{
+
+	cout << "\n List of the Customer: " << endl;
+
+	for (int i = 0; i < customerCount; i++)
+	{
+		cout << "\nCustomer's Id: " << customers[i].Id << endl;
+		cout << "Customer's Username: " << customers[i].Username << endl;
+		cout << "Customer's Password: " << customers[i].Password << endl;
+		cout << "Customer's First name: " << customers[i].First_Name << endl;
+		cout << "Customer's Last Name: " << customers[i].Last_Name << endl;
+		cout << "Customer's Address: " << customers[i].Address << endl;
+		cout << "Customer's Student Name: " << customers[i].Student_Name << endl;
+		cout << "Customer's Year of student: " << customers[i].Year_of_student << endl;
+	}
+}
+
+void CustomerMenu(CUSTOMER* customers, int& customerCount, int& maxId) {
+	CUSTOMER customer;
+
+	cout << "Id: ";
+	cin >> customer.Id;
+
+	cout << "Username: ";
+	cin >> customer.Username;
+
+	cout << "Password: ";
+	cin >> customer.Password;
+
+	cout << "First Name: ";
+	cin >> customer.First_Name;
+
+	cout << "Last Name: ";
+	cin >> customer.Last_Name;
+
+	cout << "Address: ";
+	cin >> customer.Address;
+
+	cout << "Student's name: ";
+	cin >> customer.Student_Name;
+
+	cout << "Year of Student: ";
+	while (!(cin >> customer.Year_of_student) or customer.Year_of_student < 12 and customer.Year_of_student > 16)
+	{
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "\nEnter correct Year: ";
+	}
+
+	createCustomer(customers, customerCount, customer, maxId);
+}
+void editOrderMenu(CUSTOMER* customers, int& customerCount) {
+	int customerId;
+	cout << "\nEnter car ID: ";
+	cin >> customerId;
+
+	CUSTOMER customer = getOrder(customers, customerCount, customerId);
+
+	cout << "\n1. Id: " << endl;
+	cout << "2. Username: " << endl;
+	cout << "3. Password: " << endl;
+	cout << "4. First Name: " << endl;
+	cout << "5. LastName: " << endl;
+	cout << "6 Address: " << endl;
+	cout << "7 Student Name: " << endl;
+	cout << "8 Year of student: " << endl;
+	cout << "Which field you want to edit: ";
+
+	int edit;
+	cin >> edit;
+
+	switch (edit) {
+	case 1: {
+		cout << "Id: ";
+		cin >> customer.Id;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 2: {
+		cout << "Username: ";
+		cin >> customer.Username;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 3: {
+		cout << "First Name: ";
+		cin >> customer.First_Name;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 4: {
+		cout << "Last Name: ";
+		cin >> customer.Last_Name;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 5: {
+		cout << "Address: ";
+		cin >> customer.Address;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 6: {
+		cout << "Student Name: ";
+		cin >> customer.Student_Name;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	case 7: {
+		cout << "Year of student: ";
+		cin >> customer.Year_of_student;
+		updateCustomer(customers, customerCount, customer, customerId);
+		break;
+	}
+	default: {
+		cout << "Invalid field!" << endl;
+		break;
+	}
+
+
+	}
+}
+void deleteCustomerMenu(CUSTOMER* customers, int& customerCount, int& maxId) {
+	int customerId;
+
+	cout << "Enter car Id: ";
+	cin >> customerId;
+
+	deleteCustomer(customers, customerCount, customerId);
+}
+
+bool showMainMenu(CUSTOMER* customers, int& customerCount, int& maxId) {
+
+	int choice;
+
+	cout << "\n===MAIN MENU===" << endl;
+	cout << "1. Customer Menu" << endl;
+	cout << "2. Show Customer" << endl;
+	cout << "3. Edit Customer Order" << endl;
+	cout << "4. Delete Customer Order" << endl;
+	cout << "9. Exit" << endl;
+	cout << "Your choice: ";
+
+	cin >> choice;
+
+	switch (choice) {
+	case 1: {
+		CustomerMenu(customers, customerCount, maxId);
+		break;
+	}
+	case 2: {
+		showCustomerMenu(customers, customerCount, maxId);
+		break;
+	}
+	case 3: {
+		editOrderMenu(customers, customerCount);
+		break;
+	}
+	case 4: {
+		deleteCustomerMenu(customers, customerCount, maxId);
+		break;
+	}
+
+	case 9: {
+		return false;
+	}
+	default:
+		break;
+
+	}
+}
+
 int main()
 {
-    string UsernName;
-    string pass;
+	cout << "Obshtaka Programe\n";
+	int customerCount = 0;
+	int maxId = 1;
+	CUSTOMER customers[500];
 
-    cout << "Please enter Username: ";
-    getline(cin, UsernName);
-    cout << "Please enter Password: ";
-    getline(cin, pass);
-    
-    isAdmin(UsernName, pass);
-    CUSTOMER customers[1];
- 
-        for (int i = 0; i < 1; i++)
-        {
-            input(customers[i]);
-        }
-        showCustomers(customers, 1);
+	bool mainMenu = true;
 
-    
+	do {
+		mainMenu = showMainMenu(customers, customerCount, maxId);
+	} while (mainMenu);
 }
 
